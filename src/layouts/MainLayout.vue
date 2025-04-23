50704b7d63e337c18f914e6a5c3ecaaae5a4ceab
ivan <ivanmarti.alves@gmail.com>
2025-04-23 00:19:54 -0300

    fix: add avatar ao mainlayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,22 +16,47 @@
         </q-avatar>
         <q-toolbar-title> Minha Farmacinha </q-toolbar-title>
       </q-toolbar>
-    </q-header>
+    </q-header> 
 
     <!-- Menu lateral -->
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list padding class="text-white">
-        <q-item-label header 
-        class="text-uppercase text-bold text-white text-center q-mt-7 q-mb-lg">
+        <q-item-label
+          header
+          class="text-uppercase text-bold text-white text-center q-mt-7 q-mb-lg"
+        >
           Menu
-          <!-- Mostrar nome do usuário no menu -->
-          <div v-if="user" class="text-center q-mt-sm">{{ name }}
-            <q-btn>
-              <q-avatar size="md"></q-avatar>
-                <q-icon name="person" color="white" />
-            </q-btn>
-          </div>
         </q-item-label>
+
+        <q-btn
+          flat
+          rounded
+          class="q-mb-md q-px-md q-ml-lg items-center "
+        >
+        <q-avatar
+        size="32px"
+        color="primary"
+        text-color="white"
+        class="q-mr-sm"
+        >
+        <q-icon name="person" />
+      </q-avatar>
+      <div class="text-white text-subtitle2 text-left" v-if="name">{{ name }}
+            <q-menu>
+              <q-list>
+                <q-btn 
+                flat 
+                label="Sair"
+                color="negative"
+                @click="handleUserClick"
+                />
+              </q-list>
+            </q-menu>
+
+          </div>
+        </q-btn>
+
+        <q-separator spaced color="grey-6" />
 
         <q-item clickable to="/home" exact>
           <q-item-section avatar
@@ -79,24 +104,33 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { useAuthStore } from '../stores/auth';
 import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "../stores/auth";
 
+const router = useRouter();
 const leftDrawerOpen = ref(false);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 const { name, user } = storeToRefs(authStore);
-  
+
+function  handleUserClick() {
+  if (user.value) {
+    authStore.logout();
+    void router.push('/')
+  }
+}
 </script>
 
 <style>
 .q-drawer {
   background-color: var(--gray-dark);
 }
-.q-item, .q-item-label {
+.q-item,
+.q-item-label {
   font-size: 18px;
 }
 </style>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
-import { defineStore } from '#q-app/wrappers'
 import { createPinia } from 'pinia'
+import { defineStore } from '#q-app/wrappers'
+import piniaPersist from 'pinia-plugin-persistedstate'
 
 declare module 'pinia' {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -12,6 +13,7 @@ declare module 'pinia' {
 
 export default defineStore((/* { ssrContext } */) => {
   const pinia = createPinia()
+  pinia.use(piniaPersist)
 
   // You can add Pinia plugins here
   // pinia.use(SomePiniaPlugin)
